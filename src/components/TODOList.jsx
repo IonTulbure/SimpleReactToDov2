383c2752b6c427e28812543c69ba8671a55b4b29
IonTulbure <iontulburedev@gmail.com>
2024-08-25 23:58:48 +0300

    refactor of todo_items_left btn

--- src/components/TODOList.jsx
+++ src/components/TODOList.jsx
@@ -27,12 +27,14 @@ function Item({ item, setTodos }) {
     };
 
     return (
-        <li id={item?.id} className="todo_item" onClick={completeTodo}>
-            <button className="todo_items_left">
-                <svg>
+        <li id={item?.id} className="todo_item">
+            <button className="todo_items_left" onClick={completeTodo}>
+                <svg fill={item.is_completed ? "#22C55E" : "#0d0d0d"}>
                     <circle cx="11.998" cy="11.998" fillRule="nonzero" r="9.998" />
                 </svg>
-                <p>{item?.title}</p>
+                <p style={item.is_completed ? { textDecoration: "line-through" } : {}}>
+                    {item?.title}
+                </p>
             </button>
             <div className="todo_items_right">
                 <button>
