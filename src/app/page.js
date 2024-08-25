ee544bc402f2a0aacd7f7116492cb71089521644
IonTulbure <iontulburedev@gmail.com>
2024-08-25 21:41:32 +0300

    pass todos complete & total to TODOHero component

--- src/app/page.js
+++ src/app/page.js
@@ -18,12 +18,18 @@ function Home() {
         { title: "last task", id: self.crypto.randomUUID(), is_completed: false },
     ]);
 
+    const todos_completed = todos.filter(
+        (todo) => todo.is_completed === true
+    ).length;
+
+    const total_todos = todos.length;
+
     return (
         <div className="wrapper">
             <Header />
-            <TODOHero todos_completed={0} total_todos={0} />
+            <TODOHero todos_completed={todos_completed} total_todos={total_todos} />
             <Form />
-            <TODOList todos={[todos]} />
+            <TODOList todos={todos} />
         </div>
     );
 }
